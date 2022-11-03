a4e450712a6d299303fe696748ba8a7349e14a7a
Dennis Ukonu <ukonudennis64@gmail.com>
2022-11-03 19:11:33 +0100

    --configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IEnvironmentVariables } from './dotenv.types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService =
+    app.get<ConfigService<IEnvironmentVariables>>(ConfigService);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();

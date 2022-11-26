6b0cd9c0cc87e7251b534446e8f8b9c0044946f3
Dennis Ukonu <ukonudennis64@gmail.com>
2022-11-26 20:38:06 +0100

    --add pub + priv keys

--- src/keypair/keypair.service.ts
+++ src/keypair/keypair.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { generateKeyPairSync } from 'crypto';
-
+import { createSign, createVerify } from 'crypto';
 @Injectable()
 export class KeypairService {
+  signInKeypair() {
+    const message = 'this data must be signed';
+    /// SIGN
+    const signer = createSign('rsa-sha256');
+    signer.update(message);
+    const signature = signer.sign(this.privateKey(), 'hex');
+
+    /// VERIFY
+    const verifier = createVerify('rsa-sha256');
+    verifier.update(message);
+    const isVerified = verifier.verify(this.publicKey(), signature, 'hex');
+    console.log(`Verified: ${isVerified}`);
+  }
   keyPairs() {
     const { privateKey, publicKey } = generateKeyPairSync('rsa', {
       modulusLength: 2048, // the length of your key in bits

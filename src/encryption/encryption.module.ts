d042e459e8df91a5e0a08f29ff54e0561cfb1323
devcraft1 <ukonudennis64@gmail.com>
2026-03-03 16:25:26 +0100

    fixes

--- src/encryption/encryption.module.ts
+++ src/encryption/encryption.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EncryptionService } from './encryption.service';
 import { EncryptionController } from './encryption.controller';
 import { KeyPairModule } from '../key-pair/keypair.module';
+import { AesGcmModule } from '../aes-gcm/aes-gcm.module';
 
 @Module({
-  imports: [KeyPairModule],
+  imports: [KeyPairModule, AesGcmModule],
   providers: [EncryptionService],
   controllers: [EncryptionController],
   exports: [EncryptionService],
